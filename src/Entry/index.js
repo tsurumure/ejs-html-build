e052e8e6262aa6e02806da63a836ad67553b0523
tsurumure <tsurumure@hotmail.com>
2018-01-25 15:53:57 +0800

    add jquery

--- src/Entry/index.js
+++ src/Entry/index.js
@@ -8,7 +8,7 @@ if(module.hot){
   require('ejs-render-loader!../Public/footer.ejs');
 }
 
-// var $ = require('../assets/jquery.min.js');
+// var $ = require('../Public/assets/jquery.min.js');
 // $(document).ready(function(){
 //   alert('a')
 // });
